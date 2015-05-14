0f9fe062fdb1704239b3b2bcfaad0ee2226350a1
Lyten <hikarilyten@yahoo.com>
2015-05-14 12:58:39 +0300

    Revert "The rest of the homework"
    This reverts commit de9679331f93950055b73a745112f210b1e2aaca.

--- Java-Script-Homework/JavaScript-Development-Introduction-Homework/Circle Area/script/circle-area.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-function calcCircleArea(r) {
-    return Math.PI * (r * r);
-}
-
-document.getElementById("area").innerHTML = calcCircleArea(7);
-document.getElementById("secondArea").innerHTML = calcCircleArea(1.5);
-document.getElementById("thirdArea").innerHTML = calcCircleArea(20);
